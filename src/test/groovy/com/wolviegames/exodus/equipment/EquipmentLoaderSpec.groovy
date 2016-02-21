56d0671080fffabc7ccd0794039cba3e3011fab4
wolvie <wolvie@wolviegames.com>
2016-02-20 21:11:04 -0700

    Added basic XML parsing for equipment - happy path

---
+++ src/test/groovy/com/wolviegames/exodus/equipment/EquipmentLoaderSpec.groovy
@@ -0,0 +1,36 @@
+package com.wolviegames.exodus.equipment;
+
+import spock.lang.Specification;
+
+public class EquipmentLoaderSpec extends Specification {
+
+    def setup() {
+
+    }
+
+    def "Equipment loader reads equipment from an XML file"() {
+        setup:
+        EquipmentLoader equipmentLoader = new EquipmentLoader()
+
+        when:
+        equipmentLoader.load();
+
+        then:
+        equipmentLoader.getEquipment().size() == 2;
+        equipmentLoader.getEquipment().get(0).getName() == "Equipment Name"
+        equipmentLoader.getEquipment().get(0).getDescription() == "Equipment Description"
+        equipmentLoader.getEquipment().get(0).getWeight() == 1.25
+        equipmentLoader.getEquipment().get(0).getCost() == 100
+        equipmentLoader.getEquipment().get(0).getScarcity() == Scarcity.INFREQUENT
+        equipmentLoader.getEquipment().get(0).getEquipmentCategory() == EquipmentCategory.ARMOR
+
+        equipmentLoader.getEquipment().get(1).getName() == "Equipment Name 2"
+        equipmentLoader.getEquipment().get(1).getDescription() == "Equipment Description 2"
+        equipmentLoader.getEquipment().get(1).getWeight() == 3
+        equipmentLoader.getEquipment().get(1).getCost() == 75
+        equipmentLoader.getEquipment().get(1).getScarcity() == Scarcity.RARE
+        equipmentLoader.getEquipment().get(1).getEquipmentCategory() == EquipmentCategory.MANUAL
+    }
+
+    // TODO Exceptions
+}
