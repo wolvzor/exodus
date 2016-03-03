5209f9250690ca849d35b60231245ff5544c4779
wolvie <wolvie@wolviegames.com>
2016-03-03 15:41:07 -0700

    Added barter skill to merchant result

--- src/main/groovy/com/wolviegames/exodus/barter/Merchant.groovy
+++ src/main/groovy/com/wolviegames/exodus/barter/Merchant.groovy
@@ -11,10 +11,13 @@ class Merchant {
     List<EquipmentCategory> equipmentCategories
     Map<String, Integer> barterResults = new HashMap<String, Integer>()
 
+    // Performs a barter roll result
+    // Positive represents merchant win
+    // TODO translate this to a %discount
     void performBarterRoll(String pcName, int pcBarterResult){
         // TODO create a dice roller
         int barterRollResult = (int)(Math.random() * 20) + 1 + barterSkill
-        barterResults.put(pcName,barterRollResult)
+        barterResults.put(pcName,barterRollResult - pcBarterResult)
     }
 
     int getBarterSkillResult(String pcName){
